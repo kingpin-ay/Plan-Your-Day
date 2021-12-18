ca5ffa888af97bfcd5387d085c5e96b7b79f1ea4
Ayush Mondal <ayushmondal001@gmail.com>
2021-12-19 03:22:35 +0530

    git commit -m "commit third"

--- app/src/main/java/com/ayush/planeyourday/EventCheckActivity.kt
+++ app/src/main/java/com/ayush/planeyourday/EventCheckActivity.kt
@@ -11,53 +11,55 @@ class EventCheckActivity : AppCompatActivity() {
 
     private lateinit var dRef : DatabaseReference
     private lateinit var binding: ActivityEventCheckBinding
-//    private lateinit var todoList : ArrayList<Todo>
+    private lateinit var todoList : ArrayList<Todo>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityEventCheckBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val todoList = mutableListOf(
-            Todo("chakdaha","krishnanagar","12/7/2002","2.30 PM",
-                " It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"),
-            Todo("chakdaha","krishnanagar","12/7/2002","2.30 PM",
-                " It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"),
-            Todo("chakdaha","krishnanagar","12/7/2002","2.30 PM",
-                " It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"),
-            Todo("chakdaha","krishnanagar","12/7/2002","2.30 PM",
-                " It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"),
-            Todo("chakdaha","krishnanagar","12/7/2002","2.30 PM",
-                " It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"),
-            Todo("chakdaha","krishnanagar","12/7/2002","2.30 PM",
-                " It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum")
-        )
+//        val todoList = mutableListOf(
+//            Todo("chakdaha","krishnanagar","12/7/2002","2.30 PM",
+//                " It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"),
+//            Todo("chakdaha","krishnanagar","12/7/2002","2.30 PM",
+//                " It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"),
+//            Todo("chakdaha","krishnanagar","12/7/2002","2.30 PM",
+//                " It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"),
+//            Todo("chakdaha","krishnanagar","12/7/2002","2.30 PM",
+//                " It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"),
+//            Todo("chakdaha","krishnanagar","12/7/2002","2.30 PM",
+//                " It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"),
+//            Todo("chakdaha","krishnanagar","12/7/2002","2.30 PM",
+//                " It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum")
+//        )
         binding.todoList.adapter = TodoAdapter(todoList)
         binding.todoList.layoutManager = LinearLayoutManager(this)
-//        getData()
+        getData()
     }
 
 
-//    private fun getData(){
-//        binding = ActivityEventCheckBinding.inflate(layoutInflater)
-//        dRef = FirebaseDatabase.getInstance().getReference("location")
-//        dRef.addValueEventListener(object : ValueEventListener{
-//            override fun onDataChange(p0: DataSnapshot) {
-//                if(p0.exists()){
-//                    for(locationP0 in p0.children){
-//                        val location =   locationP0.getValue(Todo::class.java)
-//                        if (location != null) {
-//                            todoList.add(location)
-//                        }
-//                    }
-//                    binding.todoList.adapter = TodoAdapter(todoList)
-//                }
-//            }
-//
-//            override fun onCancelled(p0: DatabaseError) {
-//
-//            }
-//
-//        })
-//
-//    }
+    private fun getData(){
+        binding = ActivityEventCheckBinding.inflate(layoutInflater)
+        dRef = FirebaseDatabase.getInstance(
+            "https://plane-your-day-789a8-default-rtdb.asia-southeast1.firebasedatabase.app").getReference(
+            "location")
+        dRef.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(p0: DataSnapshot) {
+                if(p0.exists()){
+                    for(locationP0 in p0.children){
+                        val location =   locationP0.getValue(Todo::class.java)
+                        if (location != null) {
+                            todoList.add(location)
+                        }
+                    }
+                    binding.todoList.adapter = TodoAdapter(todoList)
+                }
+            }
+
+            override fun onCancelled(p0: DatabaseError) {
+
+            }
+
+        })
+
+    }
 }
